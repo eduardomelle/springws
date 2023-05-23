5807720c4b5c2046f8389d2baa1f79b3f4849de5
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-05-22 21:09:51 -0300

    Atualizada a versão do Spring Boot.

--- src/main/java/br/com/eduardomelle/springws/client/gen/package-info.java
+++ src/main/java/br/com/eduardomelle/springws/client/gen/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.0 
 // Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2023.05.22 às 08:39:37 PM BRT 
+// Gerado em: 2023.05.22 às 09:03:14 PM BRT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://www.w3schools.com/xml/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
